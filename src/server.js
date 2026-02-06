f2eb1649b70da89116ffc3b02e3155d9a1722fe5
navyumm <navyum12@gmail.com>
2026-02-06 14:49:51 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const express = require("express");
+const app = express();
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+})
