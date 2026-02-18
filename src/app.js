19db253117d9b32db8cbf0a2e970d543ea37e4aa
navyumm <navyum12@gmail.com>
2026-02-18 15:21:21 +0530

    socket added

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require('socket.io');
 const cookieParser = require("cookie-parser");
-const app = express();
 const userRoutes = require("./routes/user.routes")
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server)
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "20kb", extended: true}));
 app.use(cookieParser());
 app.use(express.static("public"))
 
+app.get("/", (req, res)=> {
+    res.sendFile("../public/index.html")
+})
+
+
+io.on('connection', (socket) => {
+//   console.log('a user connected');
+
+
+socket.on("chat message", (msg)=> {
+    console.log("message : ", `${socket.id} : ${msg}`);
+
+    io.emit("chat message", `${socket.id} : ${msg}`)
+    
+})
+
+// socket.on("myMessage", (msg) => {
+//     console.log("message :", msg);
+
+//     // io.emit("mfrombackend", "hehhehehhehhhe")
+    
+// })
+
+
+});
+
 // routes
 app.use("/api/v1/users", userRoutes)
 
 
-module.exports = app;
+module.exports = server;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const db = require("./models")
-const app = require("./app")
+const server = require("./app")
 
 
 const PORT = process.env.PORT || 8000;
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 db.sequelize.sync({force: true}).then(() => {
     console.log("All models were synchronized successfully.");
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
 })
 })
